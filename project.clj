8fcf669832dc297e8a88d1674e1fdd5b52edf8b0
Jonas Lindmark <lindmark@wrapp.com>
2013-03-09 12:41:20 +0100

    First working version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chatter "0.0.1-SNAPSHOT"
+(defproject chatter "0.0.1"
   :description "Chattie chattie"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.2"]
